0f4781fae29445354b88194a04d213eec0204ccc
trishmarie002-art <trishmarie002@gmail.com>
2026-08-16 16:53:38 -0500

    Update homepage SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 export const metadata = {
-  title: "Jay's Web Design",
-  description: "Web design in San Antonio, TX",
+  title: "Affordable Web Design In TX",
+  description:
+    "Need affordable Web design in TX? Jay's Web Design Service is the best choice when looking for top quality affordable Web design in TX.",
 };
 
 export default function RootLayout({
